f730badb8a1d164a7d5a43c89f24b02ca428afa4
taylor <taylor@bowtie.works>
2025-03-13 16:05:48 -0700

    add icmp support to ingress rules

--- aws/modules/security/main.tf
+++ aws/modules/security/main.tf
@@ -25,6 +25,13 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = -1  # -1 means all ICMP types
+    to_port     = -1  # -1 means all ICMP codes
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
